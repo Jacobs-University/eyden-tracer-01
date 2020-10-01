7b3af7b69a0e0663d870bab8d751b178c9664623
mshahzaibawan <71852211+mshahzaibawan@users.noreply.github.com>
2020-10-01 20:56:50 +0200

    Update PrimPlane.h

--- src/PrimPlane.h
+++ src/PrimPlane.h
@@ -19,8 +19,8 @@ public:
 	 * @param origin Point on the plane
 	 * @param normal Normal to the plane
 	 */
-	CPrimPlane(const Vec3f& origin, const Vec3f& normal)
-		: IPrim()
+	CPrimPlane(const Vec3f& color, const Vec3f& origin, const Vec3f& normal)
+		: IPrim(color)
 		, m_normal(normal)
 		, m_origin(origin)
 	{
@@ -31,8 +31,26 @@ public:
 	virtual bool intersect(Ray& ray) const override
 	{
 		// --- PUT YOUR CODE HERE ---
-		return false;
-	}
+        float numerator = m_normal.dot(m_origin - ray.org);
+        float denominator = m_normal.dot(ray.dir);
+        float t;
+
+        if (denominator == 0){
+            return 0;
+        }
+        else{
+            t = numerator / denominator;
+        }
+
+        if (t < Epsilon || t > ray.t){
+            return false;
+        }
+
+        ray.t = t;
+
+        return true;
+    }
+		
 	
 	
 private:
