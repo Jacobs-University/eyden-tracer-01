fb18cf24fec274701eead99af724b72f2015a4dd
Otmane Sabir <o.sabir@jacobs-university.de>
2020-10-01 12:10:06 +0200

    modify build script

--- src/CameraOrthographic.cpp
+++ src/CameraOrthographic.cpp
@@ -1,2 +1,29 @@
 #include "CameraOrthographic.h"
 
+
+CameraOrthographic::~CameraOrthographic() {
+
+}
+
+CameraOrthographic::CameraOrthographic(const Size &resolution, const Vec3f &pos, const Vec3f &dir, const Vec3f &up,
+                                       float angle) : ICamera(resolution){
+    m_xAxis = normalize(m_zAxis.cross(m_up));
+    m_yAxis = normalize(m_zAxis.cross(m_xAxis));
+    m_focus = 1.0f / tanf(angle * Pif / 360);
+}
+
+void CameraOrthographic::InitRay(Ray &ray, int x, int y) {
+    Size resolution = getResolution();
+
+    // Screen-space coordinates in [-1, 1]
+    float sscx = (2 * static_cast<float>(x) / resolution.width) - 1;
+    float sscy = (2 * static_cast<float>(y) / resolution.height) - 1;
+
+    ray.org = m_pos;
+    ray.dir = normalize(getAspectRatio() * sscx * m_xAxis + sscy * m_yAxis + m_focus * m_zAxis);
+    ray.t   = std::numeric_limits<float>::infinity();
+}
+
+Vec3f CameraOrthographic::getOrigin() {
+    return cv::Vec3f();
+}
--- src/include/CameraOrthographic.h
+++ src/include/CameraOrthographic.h
@@ -4,6 +4,20 @@
 #include "ray.h"
 
 class CameraOrthographic : ICamera {
-    // --- IMPLEMENT class CCameraOrthographic ---
-    // --- PUT YOUR CODE HERE ---
+    ~CameraOrthographic();
+    CameraOrthographic(const Size& resolution, const Vec3f &pos, const Vec3f &dir, const Vec3f &up, float angle);
+
+    void InitRay(Ray &ray, int x, int y) override;
+
+    Vec3f getOrigin();
+private:
+    Vec3f m_pos;
+    Vec3f m_direction;
+    Vec3f m_up;
+    double m_size;
+
+    Vec3f m_xAxis;
+    Vec3f m_yAxis;
+    Vec3f m_zAxis;
+    double m_focus;
 };
