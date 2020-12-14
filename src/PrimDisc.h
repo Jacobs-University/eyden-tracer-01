0f69641d1dfc7c25be74b67f1328981b201784a8
sabyrrakhim06 <48181713+sabyrrakhim06@users.noreply.github.com>
2020-12-14 23:16:21 +0100

    Solution to eyden-tracer-01

--- src/PrimDisc.h
+++ src/PrimDisc.h
@@ -3,5 +3,41 @@
 #include "IPrim.h"
 #include "ray.h"
 
-// --- IMPLENET class CPrimDisc ---
+// --- IMPLEMENT class CPrimDisc ---
 // --- PUT YOUR CODE HERE ---
+class CPrimDisc: public IPrim {
+public:
+    // constructor
+    CPrimDisc(const Vec3f& origin, const Vec3f normal, double radius) : IPrim(), m_origin(origin), m_normal(normal), m_radius(radius) {}
+
+    // destructor
+    virtual ~CPrimDisc(void) = default;
+
+    // intersect method
+    virtual bool intersect(Ray& ray) const override {
+        auto plane = CPrimPlane(m_origin, m_normal);
+        Ray r_copy = ray;
+        if(!plane.intersect(r_copy)) {
+            return false;
+        }
+
+        auto pt = r_copy.org + r_copy.dir * r_copy.t;
+        auto distance = norm(pt - m_origin);
+        if(distance > m_radius) {
+            return false;
+        }
+
+        if(r_copy.t > ray.t) {
+            return false;
+        }
+
+        ray.t = r_copy.t;
+        return true;
+    }
+
+
+private:
+    Vec3f m_origin;
+    Vec3f m_normal;
+    double m_radius;
+};
