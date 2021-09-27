a9545950f6e18b47df6811fdf74f7732e48167b1
Henri Sota <sotahenry@gmail.com>
2021-09-27 13:38:09 +0200

    Update driver code

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,24 @@
 #include "PrimSphere.h"
 #include "PrimPlane.h"
 #include "PrimTriangle.h"
+#include "PrimDisc.h"
+
+void updateColorOnChange(Ray& ray, float& t_curr, Vec3f& color_curr, Vec3f color) {
+    if (t_curr != ray.t) {
+        // Update color with given color
+        t_curr = ray.t;
+        color_curr = color;
+    }
+}
 
 Mat RenderFrame(ICamera& camera)
 {
 	// scene objects
-	
 	CPrimSphere s1(Vec3f(-2, 1.7f, 0), 2);
 	CPrimSphere s2(Vec3f(1, -1, 1), 2.2f);
 	CPrimSphere s3(Vec3f(3, 0.8f, -2), 2);
 	CPrimPlane p1(Vec3f(0, -1, 0), Vec3f(0, 1, 0));
-	// Add disc primitive here
+    CPrimDisc d1(Vec3f(-2, 1.7f, 0), Vec3f(0, 1, 0), 2);
 	
 	CPrimTriangle t1(Vec3f(-2, 3.7f, 0), Vec3f(1, 2, 1), Vec3f(3, 2.8f, -2));
 	CPrimTriangle t2(Vec3f(3, 2, 3), Vec3f(3, 2, -3), Vec3f(-3, 2, -3));
@@ -21,22 +29,33 @@ Mat RenderFrame(ICamera& camera)
 	Mat img(camera.getResolution(), CV_32FC3); 	// image array
 	Ray ray;                            		// primary ray
 	
-	for(int y = 0; y< img.rows; y++)
+	for (int y = 0; y < img.rows; y++)
 		for (int x = 0; x < img.cols; x++) {
-			
 			// Initialize your ray here
-			
-			// --- PUT YOUR CODE HERE ---
+			camera.InitRay(ray, x, y);
 			
 			Vec3f col = RGB(0, 0, 0); // background color
 			
 			/*
 			 * Find closest intersection with scene
-			 * objetcs and calculate color
+			 * objects and calculate color
 			 */
-			
-			 // --- PUT YOUR CODE HERE ---
-			
+            float t_curr = ray.t;
+            s1.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(1, 0, 0));
+            d1.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(1, 0, 1));
+            s2.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(0, 1, 0));
+            s3.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(0, 0, 1));
+            p1.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(1, 1, 0));
+            t1.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(0, 1, 1));
+            t2.intersect(ray);
+            updateColorOnChange(ray, t_curr, col, RGB(1, 1, 1));
+
 			img.at<Vec3f>(y, x) = col; // store pixel color
 		}
 	
@@ -61,9 +80,9 @@ int main(int argc, char* argv[])
 	Mat img3 = RenderFrame(cam3);
 	imwrite("perspective3.jpg", img3);
 
-	// AddeEnvironmental camera here as cam4
-	// Mat img4 = RenderFrame(cam4);
-	// imwrite("orthographic4.jpg", img4);
+    CCameraEnvironmental cam4(resolution, Vec3f(-8, 3, 8), Vec3f(0, 0, 1), Vec3f(0, 1, 0));
+	Mat img4 = RenderFrame(cam4);
+	imwrite("environmental4.jpg", img4);
 
 	return 0;
 }
