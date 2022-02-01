76f15475412e306a27b39c873beed66b34097992
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-02-01 11:33:21 +0100

    Update PrimSphere.h

--- src/PrimSphere.h
+++ src/PrimSphere.h
@@ -29,8 +29,31 @@ public:
 	virtual bool intersect(Ray &ray) const override
 	{
 		// --- PUT YOUR CODE HERE ---
-		return false;
-	}
+        double r2 = static_cast<double>(m_radius) * static_cast<double>(m_radius);
+        
+        
+        Vec3f difference = ray.org - m_origin;
+        double a = static_cast<double>(ray.dir.dot(ray.dir));    //Must be equal to 1 since we normalize the ray direction.
+        double b = 2 * static_cast<double>(ray.dir.dot(difference));    //2*d*L
+        double c = static_cast<double>(difference.dot(difference)) - m_radius * m_radius;   // L^2 - r^2
+
+        double delta = b * b - 4 * a * c;       //calculation of delta
+
+        if (delta < 0) return false;    //Ray doesn't intersect
+
+        double t1 = (-b - sqrt(delta)) / 2 * a;
+        double t2 = (-b + sqrt(delta)) / 2 * a;
+
+        if (t1 > t2) std::swap(t1, t2);     //t1 is the furthuest point -> se swap them
+
+        if(t1 < 0){ //Ray is shooted from the inside of the sphere
+            t1 = t2; //We use only t2
+
+            if (t2 < 0) return false; //The ray is shooted from the opposite direction of the sphere
+        }
+
+        ray.t = t1;     //Distance is the smallest existing distance
+        return true;	}
 	
 	
 private:
